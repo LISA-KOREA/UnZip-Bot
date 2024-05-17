53d9cb1db722fcb7d877a1e097459eb3ce14c0d6
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-05-17 11:20:34 +0530

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -1,1 +1,10 @@
+import os
 
+class Config(object):
+     
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", ))
+    API_HASH = os.environ.get("API_HASH", "")
+    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    
