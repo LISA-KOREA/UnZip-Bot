bfb6e1b312704b0525471ee6a9b79b99c5532c0c
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-05-17 11:13:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ from Unzip.config import Config
 
 app = Client(
     "unzip_bot",
-    bot_token=BOT_TOKEN,
-    api_id=API_ID,
-    api_hash=API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
     plugins=dict(root="Unzip")
 )
 
