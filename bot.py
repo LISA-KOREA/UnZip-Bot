8f77326fcbd8226dc013cd5bd1d64d351df3b13a
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-05-17 11:08:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+from pyrogram import Client
+from Unzip.config import Config
 
+
+app = Client(
+    "unzip_bot",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=dict(root="Unzip")
+)
