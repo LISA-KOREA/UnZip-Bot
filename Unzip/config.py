fee68aacfcbacb3ac00c4ed7e43074e7ff37d4b3
Fan744 <135441979+Fan744@users.noreply.github.com>
2026-01-16 01:53:18 +0530

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -2,9 +2,9 @@ import os
 
 class Config(object):
      
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
-    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("7970441104:AAEiaUoJb0CG-rdz8eFDha5KYwAd6F6IfjY", "")
+    API_ID = int(os.environ.get("20401115", ))
+    API_HASH = os.environ.get("f27d722a2772cc581435be607be35022", "")
     MAX_FILE_SIZE = 2194304000
     
     
